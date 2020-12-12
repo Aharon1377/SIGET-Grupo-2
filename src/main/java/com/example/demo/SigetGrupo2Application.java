481394067cd860e021c024af2cf25c5de30bb740
Aharon1377 <aharon1377@gmail.com>
2020-12-12 13:33:41 +0100

    cors?¿?¿

--- src/main/java/com/example/demo/SigetGrupo2Application.java
+++ src/main/java/com/example/demo/SigetGrupo2Application.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ServletComponentScan
 @SpringBootApplication
 @ComponentScan("com.controller")
 @EnableMongoRepositories("com.persistence")
